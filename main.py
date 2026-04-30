ce76e046368ed74fa6c9790ea0ba67d2e8a07f9c
Sreetx <98646763+Sreetx@users.noreply.github.com>
2026-04-30 07:00:49 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -112,9 +112,9 @@ try:
                         subprocess.run(["sudo", "pacman", "-Syu"], check=True, shell=True)
                     except subprocess.CalledProcessError:
                         print(f"\n{borange} # {reset}Installing Components (APT)..."); time.sleep(0.3)
+                        subprocess.run([sys.executable, "-m", "pip", "install", "pytubefix", "ffmpeg-python"], shell=True, check=True)
                         subprocess.run(['sudo', 'apt', 'update', '&&', 'sudo', 'apt', 'upgrade'], shell=True, check=True)
                         subprocess.run(['sudo', 'apt', 'install', 'python3-requests', 'python3-prompt-toolkit', 'python3-rich', 'python3-term-image', 'python3-pillow'], shell=True, check=True)
-                        subprocess.run([sys.executable, "-m", "pip", "install", "pytubefix", "ffmpeg-python"], shell=True, check=True)
                         print(f"\n{borange} # {reset}If the installation error occurs, you can install it manually")
                         for i, h in enumerate(['Requests', 'Prompt Toolkit', 'Pytubefix', 'Rich', 'Pillow', 'Term-Image', 'FFmpeg'], 1): 
                             print(f"{hijau} {i}.{reset} {h}")
